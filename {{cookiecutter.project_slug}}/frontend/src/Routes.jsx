a72a0c2199058b1cbdb1f9aa07b4041503ba055b
William Hill <mjh2225@gmail.com>
2022-08-21 16:09:45 -0400

    updated name in package.json

--- {{cookiecutter.project_slug}}/frontend/src/Routes.jsx
+++ {{cookiecutter.project_slug}}/frontend/src/Routes.jsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import { makeStyles } from '@material-ui/core/styles';
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const Routes: FC = () => {
+export const Routes = () => {
   const classes = useStyles();
   const history = useHistory();
 
